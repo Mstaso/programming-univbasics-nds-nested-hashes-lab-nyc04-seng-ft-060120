a2332d7d7980715157d1f945e7d87ae2c0272181
malcolm staso <mstaso1@gmail.com>
2020-05-12 17:38:21 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,8 +15,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  literal_aoh = ["Don Gately"
-    {:name => 
+  literal_aoh = [
+    {:name => "Don Gately", 
   }]
 end
 
